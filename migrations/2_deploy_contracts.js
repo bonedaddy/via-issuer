581cf047fbb052ab19e7bbd3e434881950965fc2
bonedaddy <boned4ddy@protonmail.com>
2020-11-20 15:51:45 -0800

    discover unsafe contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -15,22 +15,55 @@ const { deployProxy } = require('@openzeppelin/truffle-upgrades');
 module.exports = async function(deployer, network, accounts) {
 
     await deployProxy(stringutils, {deployer});
-    // await deployer.deploy(stringutils);
     await deployer.link(stringutils, [Bond, Cash, ViaOracle]);
 
     await deployProxy(ABDKMathQuad, {deployer});
-    // await deployer.deploy(ABDKMathQuad);
     await deployer.link(ABDKMathQuad,[Cash, Bond, ViaOracle, ERC20, Token]);
 
+    // this contract is not yet upgrade safe, see https://github.com/verified-network/via-issuer/issues/38#issuecomment-731461807 for more information
+    // await deployProxy(ViaOracle, {deployer});
+    // const oracleInstance = await ViaOracle.deployed();
+    // oracleInstance.sendTransaction({from:accounts[0], to: oracleInstance.address, value:  0.25e18})
     await deployer.deploy(ViaOracle, {from: accounts[0], gas:6721975, value: 0.25e18});
 
-    await deployProxy(Cash, {deployer});
-    // await deployer.deploy(Cash);
-    await deployProxy(Bond, {deployer});
-    // await deployer.deploy(Bond);
-    await deployProxy(Token, {deployer});
-    // await deployer.deploy(Token);
+    // this contract is not yet upgrade safe, see https://github.com/verified-network/via-issuer/issues/38#issuecomment-731461807 for more information
+    // await deployProxy(Cash, {deployer});
+    await deployer.deploy(Cash);
 
+    // this contract is not yet upgrade safe, see https://github.com/verified-network/via-issuer/issues/38#issuecomment-731461807 for more information
+    // await deployProxy(Bond, {deployer});
+    await deployer.deploy(Bond);
+
+    // this contract is not yet upgrade safe, see https://github.com/verified-network/via-issuer/issues/38#issuecomment-731461807 for more information
+    // await deployProxy(Token, {deployer});
+    await deployer.deploy(Token);
+
+    /* // this contract is not yet upgrade safe, see https://github.com/verified-network/via-issuer/issues/38#issuecomment-731461807 for more information
+    await deployProxy(Factory, {deployer}).then(async () => {
+        const factory = await Factory.deployed();
+        const cash = await Cash.deployed();
+        const bond = await Bond.deployed();
+        const oracle = await ViaOracle.deployed();
+        const token = await Token.deployed();
+
+        await oracle.initialize(factory.address);
+
+        await factory.createIssuer(cash.address, web3.utils.utf8ToHex("Via_USD"), web3.utils.utf8ToHex("Cash"), oracle.address, token.address);
+        await factory.createIssuer(cash.address, web3.utils.utf8ToHex("Via_EUR"), web3.utils.utf8ToHex("Cash"), oracle.address, token.address);
+        await factory.createIssuer(cash.address, web3.utils.utf8ToHex("Via_INR"), web3.utils.utf8ToHex("Cash"), oracle.address, token.address);
+
+        await factory.createIssuer(bond.address, web3.utils.utf8ToHex("Via_USD"), web3.utils.utf8ToHex("Bond"), oracle.address, token.address);
+        await factory.createIssuer(bond.address, web3.utils.utf8ToHex("Via_EUR"), web3.utils.utf8ToHex("Bond"), oracle.address, token.address);
+        await factory.createIssuer(bond.address, web3.utils.utf8ToHex("Via_INR"), web3.utils.utf8ToHex("Bond"), oracle.address, token.address);
+
+        for (let i = 0; i < 6; i++) {
+            var factoryTokenAddress = await factory.tokens(i);
+            console.log("Token address:", factoryTokenAddress);
+            console.log("Token name:", web3.utils.hexToUtf8(await factory.getName(factoryTokenAddress)));
+            console.log("Token type:", web3.utils.hexToUtf8(await factory.getType(factoryTokenAddress)));
+            console.log();
+        }       
+    });*/
     await deployer.deploy(Factory).then(async () => {
         const factory = await Factory.deployed();
         const cash = await Cash.deployed();
@@ -58,6 +91,8 @@ module.exports = async function(deployer, network, accounts) {
     });
     
 }
+
+
 
 
 
